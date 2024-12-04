23b0e8c996ba234d12754a811eef868773b4259e
Jeff <jeffinho2002@live.com>
2024-12-04 17:10:31 -0300

    scripts
    organização e comentarios

--- scripts/transform_silver.py
+++ scripts/transform_silver.py
@@ -1,13 +1,18 @@
+# Biblioteca necessária para usar Spark
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Transform_Silver").getOrCreate()
 
-input_path = "/data/bronze/"
-output_path = "/data/silver/"
+input_path = "/data/bronze/"  # Dados brutos
+output_path = "/data/silver/"  # Dados limpos
 
+# Datasets que serão processados
 datasets = ["Pacientes", "Medicos", "Consultas", "Diagnosticos", "Prescricoes"]
 
 for dataset in datasets:
     df = spark.read.parquet(f"{input_path}/{dataset}")
-    df = df.filter(df["ID_Paciente"].isNotNull())  # Exemplo de limpeza
+    # Realiza uma limpeza nos dados, removendo registros com 'ID_Paciente' nulo
+    df = df.filter(df["ID_Paciente"].isNotNull())
+    
+    # Salva os dados limpos no diretório de saída no formato Parquet
     df.write.mode("overwrite").parquet(f"{output_path}/{dataset}")
